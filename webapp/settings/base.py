d53d1a237aac31582d219add3d48a14b684c57a0
aracelilores <aracelilores@gmail.com>
2018-05-31 15:58:16 +0200

    Update for deploying

--- webapp/settings/base.py
+++ webapp/settings/base.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(BASE_DIR)
 # PROJECT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
@@ -100,7 +101,7 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'myproject',
     }
 }
@@ -145,14 +146,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static'),
+]
 # print(f'AQUI --> {BASE_DIR}')
 # Uploaded files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded')
 MEDIA_URL = '/resource/'
 
 # Whitenoise
-WHITENOISE_ROOT = os.path.join(BASE_DIR, 'rootfiles')
+WHITENOISE_ROOT = os.path.join(PROJECT_DIR, 'rootfiles')
 
 #Login
 LOGIN_REDIRECT_URL = '/'
