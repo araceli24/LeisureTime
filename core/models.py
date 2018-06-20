d45b18b84349c306bcd5a429be210cd123bf034a
Damián <damianld@localhost.localdomain>
2018-06-21 01:03:53 +0200

    Change style nav

--- core/models.py
+++ core/models.py
@@ -62,7 +62,7 @@ class Event(TimeStampedModel):
     )
 
     # user = models.ManyToManyField(User, verbose_name='Administrador')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, verbose_name='Administrador')
     date = models.DateField('Fecha')
     date_end= models.DateField('Fecha fin',null=True, blank=True)
     time = models.TimeField('Hora',null=True, blank=True)
